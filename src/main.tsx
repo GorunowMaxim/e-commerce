93eeeb5b9337fb697b955fc1f6c449c06d8f237a
GorunowMaxim <mgorunow@gmail.com>
2024-02-25 00:57:18 +0400

    create dev

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { store } from "app/store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/e-commerce/">
             <App />
         </BrowserRouter>
     </Provider>
